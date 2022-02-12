815284d333551f528a58d7bb424fd6cfc6a2d069
Md Rakibul Islam <freelancerakibul@gmail.com>
2022-02-12 17:05:56 +0600

    css file edit only

--- functions.php
+++ functions.php
@@ -72,6 +72,11 @@ if ( ! function_exists( 'ageland_setup' ) ) :
 
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
+
+        add_theme_support( 'editor-styles' );
+
+        // Enqueue editor styles.
+        add_editor_style( 'assets/css/ageland-gutenberg.css' );
     }
 endif;
 add_action( 'after_setup_theme', 'ageland_setup' );
