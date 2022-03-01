ba0996401d59164b826ac06adca2f2b30671f420
mhsohal <mhbuetdu@gmail.com>
2022-03-01 16:40:32 +0600

    SCSS path select, Ageland services layout6

--- template-parts/services-content.php
+++ template-parts/services-content.php
@@ -9,9 +9,11 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class('col-md-3'); ?>>
     <div class="single_service_page">
         <div class="icon">
-            <?php if ( has_post_thumbnail()) :
-                the_post_thumbnail('full');
-            endif; ?>
+            <?php
+            $meta = ageland_service_meta('service_icon');
+            $met = isset($meta['id']) ? $meta['id'] : '';
+            echo wp_get_attachment_image($met);
+            ?>
             <img src="<?php echo get_template_directory_uri();?>/assets/img/service4bg.png" class="show" alt="BG" />
             <img src="<?php echo get_template_directory_uri();?>/assets/img/service4bg_hv.png" class="hide" alt="BG hover" />
         </div>
